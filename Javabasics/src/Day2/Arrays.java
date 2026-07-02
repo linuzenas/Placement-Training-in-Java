1e497e0fc37a19f1e2e91366db2ae3d3139b7f3a
linuzenas <petluzenas@gmail.com>
2026-07-02 14:39:58 +0530

    Day 2 Topics

---
+++ Javabasics/src/Day2/Arrays.java
@@ -0,0 +1,16 @@
+package Day2;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int[] marks = {85, 90, 78, 95, 88};
+
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+
+    }
+}
---
+++ Javabasics/src/Day2/OddorEven.java
@@ -0,0 +1,22 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class OddorEven {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+
+        sc.close();
+    }
+}
