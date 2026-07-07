5083bb02a46bd903226bb4089860393ad3df2db1
linuzenas <petluzenas@gmail.com>
2026-07-07 09:36:12 +0530

    Single Inheritance Day 6

---
+++ Javabasics/src/Day6/SingleInheritance.java
@@ -0,0 +1,30 @@
+package Day6;
+
+class Vehicle {
+
+	void start() {
+		System.out.println("Vehicle Started");
+	}
+
+}
+
+class Car extends Vehicle {
+
+	void drive() {
+		System.out.println("Car is Driving");
+	}
+
+}
+
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+
+		Car c = new Car();
+
+		c.start();
+		c.drive();
+
+	}
+
+}
